7dcac7ca28493465f892229713625d6aad1fba2e
shayne <79330+shayne@users.noreply.github.com>
2026-01-25 16:44:16 -0500

    test: annotate direct config test with MainActor

--- Packages/FloxBoxCore/Tests/FloxBoxCoreDirectTests/FloxBoxCoreDirectTests.swift
+++ Packages/FloxBoxCore/Tests/FloxBoxCoreDirectTests/FloxBoxCoreDirectTests.swift
@@ -1,6 +1,7 @@
 @testable import FloxBoxCoreDirect
 import XCTest
 
+@MainActor
 final class FloxBoxCoreDirectTests: XCTestCase {
     func testDirectConfigLabel() {
         XCTAssertEqual(FloxBoxDirectServices.configuration().label, "Direct")
